89dc66ff9c14d9277e70c4543891256328fdefa6
PabloCoti <pca2003gte@gmail.com>
2024-04-26 23:52:59 -0600

    Diseño de interfaz e inicialización de interfaz

--- vista.py
+++ vista.py
@@ -1,9 +1,17 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
-from PyQt5 import uic
 from PyQt5.QtCore import QTimer, QTime, pyqtSignal
+from PyQt5.QtGui import QColor
 from threading import Thread
+from PyQt5 import uic
 import time
+import sys
+
+from PyQt5.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QLabel,
+    QHeaderView,
+    QTableWidgetItem,
+)
 
 
 def obtener_hora(ventana):
@@ -18,12 +26,21 @@ class VentanaPrincipal(QMainWindow):
 
     def __init__(self):
         super(VentanaPrincipal, self).__init__()
-        uic.loadUi('interfaz.ui', self)
+        uic.loadUi("interfaz.ui", self)
 
         # iniciar hilo para la hora
         self.hilo_hora = Thread(target=obtener_hora, args=(self,))
         self.hilo_hora.daemon = True  # se hace un hilo "demonio", se ejecuta en segundo plano y finaliza al cerrar
-                                      # la ventana principal
+
+        # inicializar widgets
+        for row in range(2):
+            text = "S.O." if row == 0 else ""
+            self.tablaMemoria.setItem(row, 0, QTableWidgetItem(text))
+            self.tablaMemoria.item(row, 0).setBackground(QColor("skyblue"))
+
+        self.tablaMemoria.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+
+        # la ventana principal
         self.hilo_hora.start()
 
         # conectar hilo a la señal del método que sirve para actualizar la hora
