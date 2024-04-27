a1171c0bda91b64919374e9c8957ecac85bf7a60
PabloCoti <pca2003gte@gmail.com>
2024-04-27 00:41:21 -0600

    Inicialización de proceso para round robin

--- vista.py
+++ vista.py
@@ -1,4 +1,5 @@
-from PyQt5.QtCore import QTimer, QTime, pyqtSignal
+from PyQt5.QtCore import QTime, pyqtSignal
+from roundRobin import proceso
 from PyQt5.QtGui import QColor
 from threading import Thread
 from PyQt5 import uic
@@ -6,11 +7,11 @@ import time
 import sys
 
 from PyQt5.QtWidgets import (
-    QApplication,
-    QMainWindow,
-    QLabel,
-    QHeaderView,
     QTableWidgetItem,
+    QApplication,
+    QHeaderView,
+    QMessageBox,
+    QMainWindow,
 )
 
 
@@ -25,6 +26,9 @@ class VentanaPrincipal(QMainWindow):
     actualizar_hora = pyqtSignal(str)
 
     def __init__(self):
+        self.data = None
+        self.time_start = None
+
         super(VentanaPrincipal, self).__init__()
         uic.loadUi("interfaz.ui", self)
 
@@ -34,8 +38,7 @@ class VentanaPrincipal(QMainWindow):
 
         # inicializar widgets
         for row in range(2):
-            text = "S.O." if row == 0 else ""
-            self.tablaMemoria.setItem(row, 0, QTableWidgetItem(text))
+            self.tablaMemoria.setItem(row, 0, QTableWidgetItem("S.O."))
             self.tablaMemoria.item(row, 0).setBackground(QColor("skyblue"))
 
         self.tablaMemoria.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
@@ -43,12 +46,71 @@ class VentanaPrincipal(QMainWindow):
         # la ventana principal
         self.hilo_hora.start()
 
+        # interacciones de usuario
+        self.pushButtonIniciar.clicked.connect(self.iniciar_procesos)
+        self.botonEstablecer.clicked.connect(self.establecer_cantidad_procesos)
+
         # conectar hilo a la señal del método que sirve para actualizar la hora
         self.actualizar_hora.connect(self.actualizar_label_hora)
 
     def actualizar_label_hora(self, hora):
         # actualizar el label con la hora
         self.lblHora.setText(hora)
+
+    def actualizar_tabla_memoria(self):
+        # initializes on 2 because OS occupies 2 memory slots
+        total_rows = 2
+        self.data = sorted(self.data, key=lambda x: x.llegada)
+
+        for data in self.data:
+            for i in range(total_rows, int(data.rafaga) + total_rows):
+                if total_rows <= 15:
+                    self.tablaMemoria.setItem(i, 0, QTableWidgetItem(data.id))
+                    self.tablaMemoria.item(i, 0).setBackground(QColor("skyblue"))
+
+                    total_rows += 1
+
+                else:
+                    QMessageBox.critical(self, "Error", "La memoria está llena")
+
+    def ejecutar_round_robin(self):
+        pass
+
+    def iniciar_procesos(self):
+        def check_tablaProcesos():
+            for row in range(self.tablaProcesos.rowCount()):
+                for column in range(self.tablaProcesos.columnCount()):
+                    item = self.tablaProcesos.item(row, column)
+                    if item is None or item.text() == "":
+                        return False
+            return True
+
+        # verificar si tabla no esta vacia
+        if check_tablaProcesos:
+            self.data = []
+            self.time_start = QTime.currentTime()
+
+            for row in range(self.tablaProcesos.rowCount()):
+                id = self.tablaProcesos.item(row, 0).text()
+                rafaga = self.tablaProcesos.item(row, 2).text()
+                llegada = self.tablaProcesos.item(row, 1).text()
+
+                self.data.append(proceso(id, rafaga, llegada))
+
+            self.tablaProcesos.setEnabled(False)
+            self.pushButtonIniciar.setEnabled(False)
+
+            self.actualizar_tabla_memoria()
+            self.ejecutar_round_robin()
+
+    def establecer_cantidad_procesos(self):
+        cantidad_procesos = int(self.inputCantidadP.text())
+
+        self.tablaResultados.setRowCount(cantidad_procesos)
+        self.tablaProcesos.setRowCount(cantidad_procesos)
+        self.groupCantidadP.setEnabled(False)
+        self.tablaMemoria.setEnabled(False)
+        self.widget.setEnabled(True)
 
 
 if __name__ == "__main__":
